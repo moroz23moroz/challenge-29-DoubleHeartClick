3dbc608a998edc9c4c6d36435cf4d295707f9399
Marina Moroz <moroz2387moroz@yandex.ru>
2024-11-07 12:51:48 +1000

    add script

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,55 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
+window.addEventListener("DOMContentLoaded", () => {
+  const loveMe = document.querySelector(".love__me");
+  const times = document.querySelector("#times");
 
+  let clickTime = 0;
+  let timesClicked = 0;
+
+  loveMe.addEventListener("click", (e) => {
+    if (clickTime === 0) {
+      clickTime = new Date().getTime();
+    } else {
+      if (new Date().getTime() - clickTime < 800) {
+        createHeart(e);
+        clickTime = 0;
+        loveMe.style.transform = "scale(1.1)";
+        setTimeout(() => {
+          loveMe.style.transform = "scale(1.05)";
+        }, 200);
+        loveMe.addEventListener("mouseover", () => {
+          loveMe.style.transform = "scale(1.05)";
+        });
+
+        loveMe.addEventListener("mouseout", () => {
+          loveMe.style.transform = "scale(1)";
+        });
+      } else {
+        clickTime = new Date().getTime();
+      }
+    }
+  });
+
+  const createHeart = (e) => {
+    const heart = document.createElement("i");
+    heart.classList.add("fas");
+    heart.classList.add("fa-heart");
+
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const leftOffset = e.target.offsetLeft;
+    const topOffset = e.target.offsetTop;
+
+    const xInside = x - leftOffset;
+    const yInside = y - topOffset;
+
+    heart.style.top = `${yInside}px`;
+    heart.style.left = `${xInside}px`;
+
+    loveMe.appendChild(heart);
+
+    times.innerHTML = ++timesClicked;
+
+    setTimeout(() => heart.remove(), 1000);
+  };
+});
